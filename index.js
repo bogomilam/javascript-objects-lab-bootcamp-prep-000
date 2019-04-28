a5f4e077bd1d847d7dc254c1ae56f4a469f65073
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-28 18:52:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {spices:"paprika"}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  recipes["spices"]="paprika"
+  return recipes
+}
+
+function updateObjectWithKeyAndValue(object,key,value){
+  return Object.assign({},object,{[key] : value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+recipes["spices"]="paprika"
+return recipes
+}
