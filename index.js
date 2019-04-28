f3379a3dfe86a3ec08d44daca5539e62172aeaff
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-28 19:28:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key, value){
 
 function deleteFromObjectByKey(object, key){
 var newObj = Object.assign({}, obj)
-delete object.[key];
+delete object[key];
 return newObj;
 }
